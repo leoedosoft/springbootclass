7299ca2723728f7bc86760ca3b42db7e399dbf12
condorit <juancondorg@hotmail.it>
2024-02-22 12:10:40 +0100

    jc-01 moduli user blog and jwt

--- blog-data-api/src/main/java/com/teoresi/blogdataapi/repository/IRoleRepository.java
+++
@@ -1,8 +0,0 @@
-package com.teoresi.blogdataapi.repository;
-
-import com.teoresi.blogdataapi.model.Role;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface IRoleRepository extends JpaRepository<Role, String> {
-
-}
